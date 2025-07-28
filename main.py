1842530eb49184eb8b7291f2da9386c99db8d2b0
Edwardo Malta <edwardoalta@gmail.com>
2025-07-28 13:46:25 -0600

    builded and tested kdev

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+import os, sys
+from kivy.resources import resource_add_path
+
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.uix.image import Image
+from kivy.properties import ListProperty, NumericProperty
+from kivy.metrics import dp
+from kivy.core.window import Window
+from kivy.uix.label import Label
+
 
 
 kv = """
@@ -9,7 +18,22 @@ BoxLayout:
     orientation: "vertical"
     ItemLog:
     ItemLog:
-
+<CLabel>:
+    #font_size: sp(12)
+    #size_hint_y: None
+    #height: self.texture_size[1] + dp(3)
+    canvas.before:
+        Color:
+            rgba: 0, 0, 0, 0.5
+        RoundedRectangle:
+            pos: self.x - dp(2), self.y - dp(2)
+            size: self.size
+            radius: [dp(5),]
+        Color:
+            rgba: 0, 0, 1, 1
+        RoundedRectangle:
+            pos: self.pos
+            size: self.size
 <BLabel@Label>:
     font_size: sp(12)
     size_hint_y: None
@@ -27,30 +51,34 @@ BoxLayout:
             pos: self.pos
             size: self.size
 
+<TLabel@Label>:
+    size_hint_y: None
+    height: self.texture_size[1] + dp(3)
+
 <ItemLog@RelativeLayout>:
     Image:
         id: bg_image 
         source: "Frame.png"
-        allow_stretch: True
-        keep_ratio: True
+        fit_mode: "contain"
+        size_hint_min: dp(340 + 30), dp(272 + 30)
     BoxLayout:
         orientation: "vertical"
-        size_hint_x: None
-        width: dp(bg_image.width)
-        pos_hint: {"center_x": 0.5 }
+        size_hint: None, None
+        size: bg_image.norm_image_size
+        pos_hint: {"center_x": 0.5, "center_y": 0.5 }
         Widget:
             size_hint_y: None
             height: dp(20)
         BoxLayout:
             orientation: "vertical"
             Widget:
-            Label:
+            TLabel:
                 text: "TITLE LABEL"
                 font_size: sp(20)
-            Label:
+            TLabel:
                 text: "Subtitle"
                 font_size: sp(14)
-            Label:
+            TLabel:
                 text: "Date time: 07/07/2025 14:30"
                 font_size: sp(12)
             Widget:
@@ -58,24 +86,44 @@ BoxLayout:
             cols: 2
             padding: dp(15)
             spacing: dp(5)
-            BLabel:
+            CLabel:
                 text: "Name"
-            BLabel:
+            CLabel:
                 text: "14. Geographic Z A"
-            BLabel:
+            CLabel:
                 text: "Zone"
-            BLabel:
+            CLabel:
                 text: "New area Z"
-            BLabel:
+            CLabel:
                 text: "User Name"
-            BLabel:
-                text: "Jonh Fritz"
+            CLabel:
+                text: "Jonh Frutz"
 
 
 """
 
+class CLabel(Label):
+    min_font_size = NumericProperty(5)
+
+    def on_size(self, *args):
+        t_width, t_height= self.texture_size
+        width, height = self.size
+        if t_height < height and t_width < width:  # Grow
+            while t_height < height and t_width < width:
+                self.font_size += 1
+                self.texture_update()
+                t_width,t_height = self.texture_size
+        elif t_height > height or t_width > width:  # shrink
+            while t_height > height or t_width > width:
+                self.font_size = max(self.min_font_size, self.font_size - 1)
+                if self.font_size == self.min_font_size:
+                    break
+                self.texture_update()
+                t_width, t_height = self.texture_size
+
 class ExampleApp(App):
     def build(self):
+        Window.minimum_width, Window.minimum_height = (dp(450), dp(665))
         return Builder.load_string(kv)
 
 ExampleApp().run()
